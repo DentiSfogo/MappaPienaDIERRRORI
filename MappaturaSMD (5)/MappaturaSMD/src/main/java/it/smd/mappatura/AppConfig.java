59ca5a5f1139b84be20478942366f96cc2ecac58
DentiSfogo <47703723+DentiSfogo@users.noreply.github.com>
2026-01-28 21:26:50 +0100

    Set default ingest key for operators

--- MappaturaSMD (5)/MappaturaSMD/src/main/java/it/smd/mappatura/AppConfig.java
+++ MappaturaSMD (5)/MappaturaSMD/src/main/java/it/smd/mappatura/AppConfig.java
@@ -30,7 +30,7 @@ public class AppConfig {
     public static AppConfig defaults() {
         AppConfig c = new AppConfig();
         c.endpointUrl = "https://mappatura-smd-8dad3f3c.base44.com";
-        c.ingestKey = "";
+        c.ingestKey = "SMD_INGEST_9f3a8c2e71b44d9abcf20e11a7d8c6b5";
         c.bearerToken = "";
         c.sessionCode = "";
         c.autoStart = false;
